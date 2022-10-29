b877b0f70b8def0f10cd18838b59f7a1f1847cb0
mohammad6202 <112093649+mohammad6202@users.noreply.github.com>
2022-10-29 20:39:01 +0300

    u

---
+++ lib/logn_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Login_screen extends StatefulWidget {
+  const Login_screen({super.key});
+
+  @override
+  State<Login_screen> createState() => _Login_screenState();
+}
+
+class _Login_screenState extends State<Login_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+import 'home.dart';
+import 'logn_page.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: handleAuthState(),
+    );
+  }
+}
+
+handleAuthState() {
+  return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (BuildContext context, snapshot) {
+        if (snapshot.hasData) {
+          return Home();
+        } else {
+          return const Login_screen();
+        }
+      });
+}
